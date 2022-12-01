e866753abad11b59294b34e89204bd4637f53d74
Patrick Dassier <patrick.dassier@legrand.fr>
2022-12-01 09:15:48 +0100

    Cleaning

--- src/ui/volumeslider.py
+++ src/ui/volumeslider.py
@@ -5,16 +5,19 @@ from ui.slider import BaseSlider
 
 
 class VolumeSlider (BaseSlider):
+    MAX = 20
+    MIN = -50
+
     def __init__(self, parent: Optional[QWidget] = None, f: Qt.WindowType = Qt.WindowType.Widget) -> None:
         super().__init__(parent, f)
-        self.slider.setMaximum(20)
-        self.slider.setMinimum(-50)
+        self.slider.setMaximum(self.MAX)
+        self.slider.setMinimum(self.MIN)
         self.slider.setValue(0)
         self.label.setText('0')
 
     @Slot(int)
     def setLabelValue(self, value):
-        if value == -50:
+        if value <= self.MIN:
             value = '-\u221E'
         self.label.setText(str(value))
         self.adjustSize()

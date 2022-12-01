23bb08452b36a1c536359ace554ca355a5a35977
Patrick Dassier <patrick.dassier@legrand.fr>
2022-12-01 15:38:53 +0100

    Fix.

--- src/ui/slider.py
+++ src/ui/slider.py
@@ -1,9 +1,12 @@
-from PySide6.QtWidgets import QWidget, QVBoxLayout, QSlider, QLabel
+from PySide6.QtWidgets import QWidget, QVBoxLayout, QSlider, QLineEdit
 from PySide6.QtCore import Qt, Slot
 from typing import Optional
 
 
 class BaseSlider(QWidget):
+
+    MAX = 1
+    MIN = 0
 
     def __init__(self, parent: Optional[QWidget] = None, f: Qt.WindowType = Qt.WindowType.Widget) -> None:
         super().__init__(parent, f)
@@ -12,14 +15,22 @@ class BaseSlider(QWidget):
         vBox.setSizeConstraint(QVBoxLayout.SizeConstraint.SetMinimumSize)
         self.slider = QSlider(Qt.Orientation.Vertical)
         self.slider.setTickPosition(QSlider.TickPosition.TicksLeft)
-        self.label = QLabel()
-        self.label.setAlignment(Qt.AlignmentFlag.AlignRight)
+        self.slider.setMaximum(self.MAX)
+        self.slider.setMinimum(self.MIN)
+        self.label = QLineEdit()
+        self.label.setAlignment(Qt.AlignmentFlag.AlignCenter)
         vBox.addWidget(self.slider)
         vBox.addWidget(self.label)
+        vBox.setAlignment(Qt.AlignmentFlag.AlignCenter)
         self.slider.valueChanged.connect(self.setLabelValue)
+        self.label.textEdited.connect(self.setSliderValue)
 
     @Slot(int)
-    def setLabelValue(self, value):
+    def setSliderValue(self, value: str):
+        self.slider.setValue(int(value))
+
+    @Slot(int)
+    def setLabelValue(self, value: int):
         self.label.setText(str(value))
         self.label.adjustSize()
 

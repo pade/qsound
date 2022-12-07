e830c3c532bda693c7ac8c02000eb79aef7367d8
Patrick Dassier <patrick.dassier@legrand.fr>
2022-12-07 12:30:10 +0100

    New slider

--- src/ui/slider.py
+++ src/ui/slider.py
@@ -1,29 +1,25 @@
-from PySide6.QtWidgets import QWidget, QVBoxLayout, QSlider, QLineEdit
+from PySide6.QtWidgets import QWidget, QVBoxLayout, QSlider, QLineEdit, QSizePolicy, QLabel
 from PySide6.QtCore import Qt, Slot
 from typing import Optional
+from designer.slider import Ui_baseSlider
 
 
-class BaseSlider(QWidget):
+class BaseSlider(QWidget, Ui_baseSlider):
 
     MAX = 1
     MIN = 0
 
-    def __init__(self, parent: Optional[QWidget] = None, f: Qt.WindowType = Qt.WindowType.Widget) -> None:
+    def __init__(self, label: str = None, parent: Optional[QWidget] = None, f: Qt.WindowType = Qt.WindowType.Widget) -> None:
         super().__init__(parent, f)
+        self.setupUi(self)
         vBox = QVBoxLayout()
         self.setLayout(vBox)
-        vBox.setSizeConstraint(QVBoxLayout.SizeConstraint.SetMinimumSize)
-        self.slider = QSlider(Qt.Orientation.Vertical)
-        self.slider.setTickPosition(QSlider.TickPosition.TicksLeft)
+        if label:
+            self.label.setText(label)
         self.slider.setMaximum(self.MAX)
         self.slider.setMinimum(self.MIN)
-        self.label = QLineEdit()
-        self.label.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        vBox.addWidget(self.slider)
-        vBox.addWidget(self.label)
-        vBox.setAlignment(Qt.AlignmentFlag.AlignCenter)
         self.slider.valueChanged.connect(self.setLabelValue)
-        self.label.textEdited.connect(self.setSliderValue)
+        self.valueEdit.textEdited.connect(self.setSliderValue)
 
     @Slot(int)
     def setSliderValue(self, value: str):
@@ -31,13 +27,12 @@ class BaseSlider(QWidget):
 
     @Slot(int)
     def setLabelValue(self, value: int):
-        self.label.setText(str(value))
-        self.label.adjustSize()
+        self.valueEdit.setText(str(value))
 
     @Slot(int)
     def setValue(self, value):
         self.slider.setValue(value)
-    
+
     def value(self):
         return self.slider.value()
 
